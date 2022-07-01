8093b84d4ff26a6ae896df79487b8c55b0646e05
vekstc <vekstc@gmail.com>
2022-07-01 18:29:32 +0200

    refector & clean test

--- src/components/Calendar/Calendar.test.ts
+++ src/components/Calendar/Calendar.test.ts
@@ -1,29 +1,35 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, VueWrapper } from "@vue/test-utils";
+import { ComponentPublicInstance } from "vue";
 import Calendar from "./Calendar.vue";
 
 describe("Calendar", () => {
+  const createWrapper = (): VueWrapper<ComponentPublicInstance> =>
+    shallowMount(Calendar);
+
   it("displays correct date", () => {
-    const wrapper = shallowMount(Calendar);
+    const wrapper = createWrapper();
 
     expect(wrapper.text()).toContain("Jul 2022");
   });
 
   it("shows correct number of days", () => {
-    const wrapper = shallowMount(Calendar);
+    const wrapper = createWrapper();
 
     expect(wrapper.findAll("ul > li").length).toBe(35);
   });
 
-  it("changes to previous month", async () => {
-    const wrapper = shallowMount(Calendar);
+  it("changes and displays previous month", async () => {
+    const wrapper = createWrapper();
+
     const nextMonthButton = wrapper.find("[data-testid='prevMonth']");
     await nextMonthButton.trigger("click");
 
     expect(wrapper.find("h2").text()).toContain("Jun 2022");
   });
 
-  it("changes to next month", async () => {
-    const wrapper = shallowMount(Calendar);
+  it("changes and displays next month", async () => {
+    const wrapper = createWrapper();
+
     const nextMonthButton = wrapper.find("[data-testid='nextMonth']");
     await nextMonthButton.trigger("click");
 
